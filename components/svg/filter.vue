4447e3776e8c86b15d666da835b1ce172cf2ed8d
RP1minus1 <robin.pillar@1minus1.com>
2023-05-05 09:59:02 +0100

    added svg filter effect

---
+++ components/svg/filter.vue
@@ -0,0 +1,41 @@
+<script setup>
+// Template Refs
+const displacementMapRef = ref(null);
+
+// Expose
+defineExpose({
+  displacementMapRef,
+});
+</script>
+
+<template>
+  <svg
+    viewBox="0 0 100 100"
+    color-interpolation-filters="sRGB"
+    class="hidden w-full"
+  >
+    <defs>
+      <filter id="svgFilter__001" x="0%" y="0%" width="100%" height="100%">
+        <feTurbulence
+          type="fractalNoise"
+          baseFrequency="0.025"
+          numOctaves="1"
+          result="turbulence"
+        ></feTurbulence>
+        <feDisplacementMap
+          in2="turbulence"
+          in="SourceGraphic"
+          ref="displacementMapRef"
+          scale="100"
+          xChannelSelector="R"
+          yChannelSelector="G"
+        ></feDisplacementMap>
+        <feComposite
+          operator="in"
+          in="SourceGraphic"
+          result="intermediate-1"
+        ></feComposite>
+      </filter>
+    </defs>
+  </svg>
+</template>
---
+++ pages/svg-filter.vue
@@ -0,0 +1,39 @@
+<script setup>
+import gsap from "gsap";
+// Template Refs
+const svgFilterRef = ref(null);
+
+onMounted(() => {
+  console.log(svgFilterRef.value.displacementMapRef);
+
+  gsap.to(svgFilterRef.value.displacementMapRef, {
+    duration: 2,
+    attr: {
+      scale: 0,
+    },
+    ease: "power2.inOut",
+  });
+});
+</script>
+
+<template>
+  <div
+    class="flex h-screen w-screen flex-col items-center justify-center bg-teal-500"
+  >
+    <svg-filter ref="svgFilterRef" />
+    <div class="container flex flex-col items-center justify-center">
+      <h1
+        class="mb-20 w-8/12 text-center text-8xl font-semibold text-gray-50"
+        style="filter: url('#svgFilter__001')"
+      >
+        SVG Filter Effect
+      </h1>
+      <h2
+        class="w-6/12 text-center text-4xl font-semibold text-gray-50"
+        style="filter: url('#svgFilter__001')"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+      </h2>
+    </div>
+  </div>
+</template>
